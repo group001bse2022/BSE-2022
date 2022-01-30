9e5d7e1982c1eac166e5677c36a6264c1835a971
github_username <email_address>
2022-01-30 12:45:07 -0800

    :assignment 2

---
+++ src/chapter2/exercise3.py
@@ -0,0 +1,4 @@
+hour = int(input("Enter number of hours :"))
+rate = float(input("Enter rate per hours :"))
+pay = hour*rate
+print("pay", pay)
---
+++ src/chapter2/exercise5.py
@@ -0,0 +1,6 @@
+celsius = float(input("Enter temperature"))
+fahrenheit = float((celsius*9/5)+32)
+#converted temperature
+print("fahrenheit", fahrenheit)
+
+0
